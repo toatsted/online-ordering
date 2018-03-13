d608d132b3c008d83fd1f8d24c71593f37d05410
Sam <samueljacksonhicks@gmail.com>
2018-03-12 23:51:56 -0500

    Dish constructor and Menu object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,28 @@
 $(() => {
 	
+	function Dish(name, price, ingredients){
+		this.name = name;
+		this.price = price;
+		this.ingredients = ingredients;
+	}
+
+	let Menu = {
+		main: [],
+		appetizers: [],
+		desserts: [],
+		drinks: []
+	}
+	
+	Menu.drinks.push(new Dish(
+		name = "coke",
+		price = "1.00",
+		ingredients = "coke ingredients"
+	));
+
+	Menu.drinks.forEach((value, index) => {
+		console.log(value.name);
+		console.log(value.price);
+		console.log(ingredients);
+	})
+
 });

0cfa452ab83c1663bc8cf42b468a53713084a8ef
Sam <samueljacksonhicks@gmail.com>
2018-03-15 16:45:30 -0500

    backed up index.html and index.js. firebase kinda works maybe

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,63 +1,75 @@
 $(() => {
+	let storage = firebase.storage();
+	let database = firebase.database();
 	$("input").attr("autocomplete", "off");
+
+	database.ref("menu/").remove();
 
 	// Dish constructor
 	function Dish(menu, name, price, ingredients) {
-
 		this.menu = menu;
 		this.name = name;
 		this.price = price;
 		this.ingredients = ingredients;
-		// add this Dish to the propper menu
-		Menu[this.menu].push(this);
 	}
 
-	// Menu object
-	let Menu = {
+	 
+	database.ref("menu/").on("value", (snapshot) => {
+		let data = snapshot.val();
 
-		main: [],
-		starters: [],
-		 : [],
-		drinks: [],
-
-		display() {
-			$(".menu").empty();
-			for (let key in Menu) {
-				if(Array.isArray(Menu[key])){
-					Menu[key].forEach((value) => {
-						$("#" + key).append($("<ul>").addClass("dish")
-							.append($("<h3>").addClass("dish-name").text(value.name))
-							.append($("<li>").text("price: $" + value.price))
-							.append($("<li>").text("ingredients: " + value.ingredients.join(", "))));
-					})
-				}
+		if(!data){
+			data = {
+				main: [],
+				starters: [],
+				desserts: [],
+				drinks: []
 			}
 		}
-	}
 
-	// Add a Dish form handler
-	$("#add-dish").on("submit", (event) => {
-		event.preventDefault();
+		$(".menu").empty();
+		for(key in data){
+			data[key].forEach((value, index) => {
+				$("#" + key).append($("<div>").addClass("dish").attr("data-index", index).attr("data-menu", value.menu)
+					.append($("<ul>"))
+					.append($("<h3>").addClass("dish-name").text(value.name))
+					.append($("<li>").text("price: $" + value.price))
+					.append($("<li>").text("ingredients: " + value.ingredients.join(", "))));
+			})
+		}
 
-		/* this is a mess of turnary operators that shouldn't exist yet im
-		also kinda proud of it */
-		let menu = ($("#main-button").prop("checked")) ? "main" : 
-			($("#starters-button").prop("checked")) ? "starters" : 
-			($("#desserts-button").prop("checked")) ? "desserts" : "drinks";
+		$("#add-dish").off("submit").on("submit", (event) => {
+			event.preventDefault();
+
+			let menu = ($("#main-button").prop("checked")) ? "main" : 
+				($("#starters-button").prop("checked")) ? "starters" : 
+				($("#desserts-button").prop("checked")) ? "desserts" : "drinks";
+
+			if(!data[menu]){
+				data[menu] = [];
+			}
+
+			data[menu].push(
+				new Dish(menu, $("#name-input").val(),
+					$("#price-input").val(),
+					$("#ingredients-input").val().split("+")
+				)
+			);
+
+			database.ref("menu/").set(data);
+		})
+
+		$(".menu").off("click").on("click", ".dish", function(event) {
+			let btn = $(this);
 			
-		new Dish(menu, $("#name-input").val(),
-			$("#price-input").val(),
-			$("#ingredients-input").val().split("+"));
+			database.ref("menu/" + btn.attr("data-menu") + "/" + btn.attr("data-index")).remove();
 
-		$("input").val("");
-		Menu.display(); 
+			for(key in data){
+				data[key].forEach((value, index) => {
+					$("#" + key).find("div").attr("data-index", index);
+				}) 
+			}
+			
+		})
+
 	})
-
-	new Dish("drinks", "coke", 1.50, ["coke", "ice"]);
-	new Dish("starters", "kimchi fries", 3, ["kimchi", "fries"]);
-	new Dish("main", "hamburger", 5.50, ["beef", "buns", "lettuce", "tomato"]);
-	new Dish("drinks", "coffee", .75, ["ground coffee beans", "water"]);
-
-	Menu.display();
 });
-

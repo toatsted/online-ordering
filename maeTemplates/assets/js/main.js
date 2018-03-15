2007a449e678cf65b8e5f1015e5cb41d70864193
Maeli Hector <maelihector@gmail.com>
2018-03-15 13:43:09 -0500

    Add caorusel to menu.html with placeholder photos. Add jQuery Initialization for caorusel to main.js.

--- maeTemplates/assets/js/main.js
+++ maeTemplates/assets/js/main.js
@@ -1,5 +1,5 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
@@ -7,7 +7,11 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$(document).ready(function(){
+$(document).ready(function () {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
+});
+
+$(document).ready(function () {
+  $('.carousel').carousel();
 });

a10f81f43179cf35af650c42357f0e04b19aaef0
Github Education <writemikehong@gmail.com>
2018-03-19 23:12:24 -0500

    seconditeration

--- maeTemplates/assets/js/main.js
+++ maeTemplates/assets/js/main.js
@@ -1,23 +1,103 @@
-(function ($) {
-  $(function () {
+var olo = window.olo || {};
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+olo.checkIfLarge = {
+    init: function() {
+        var sizeElem = $('.hide-small').css('display');
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+        return sizeElem == 'block' ? true : false;
+    }
+}
 
-$(document).ready(function () {
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
-});
+olo.headerColor = {
+    init: function() {
+        var header = $('header');
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 0) {
+                $('body').addClass('js-header-scrolled');
+            } else {
+                $('body').removeClass('js-header-scrolled');
+            }
+        });
+    }
+};
 
-$(document).ready(function () {
-  $('.carousel').carousel();
-});
+olo.hideOverScroll = {
+    init: function() {
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+                $('body').addClass('page-bottom');
+            } else {
+                $('body').removeClass('page-bottom');
+            }
+        })
+    }
+}
 
-// Initialize collapse button
-$(".button-collapse").sideNav();
-// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-//$('.collapsible').collapsible();
+// Toggle mobile navigation modal
+olo.mobileNav = {
+    init: function() {
+        $('#mobile-nav-trigger').on('click', function(event) {
+            event.preventDefault();
+            toggleNav();
+        });
+
+        $(document).keyup(function(event) {
+            if (event.keyCode == 27) {
+                closeNav();
+            }
+        });
+
+        function toggleNav() {
+            $('body').toggleClass('js-nav-visible');
+        }
+
+        function closeNav() {
+            $('body').removeClass('js-nav-visible');
+        }
+    }
+};
+
+// Scroll to position on page
+olo.scrollNav = {
+    init: function() {
+        $('#nav-global-menu').on('click', '.js-scroll-link', function(event) {
+            var target = $(this).data('target');
+            $('body').toggleClass('js-nav-visible');
+            $('html, body').animate({
+                scrollTop: $('#' + target).offset().top
+            }, 200);
+        });
+    }
+};
+
+olo.modal = {
+    init: function() {
+        $('body').on('click', '.js-modal-trigger', function(event) {
+            var modalTarget = $(this).data('modal');
+            event.preventDefault();
+            showModal(modalTarget);
+        });
+
+        $('.modal').on('click', '.js-modal-hide', function(event) {
+            hideModal();
+        });
+
+        $(document).keyup(function(event) {
+            if (event.keyCode == 27) {
+                hideModal();
+            }
+        });
+
+        function showModal(target) {
+            var modalTarget = target;
+            $('body').addClass('js-modal-visible');
+            $('#' + target).addClass('js-modal-shown');
+        }
+
+        function hideModal() {
+            $('body').removeClass('js-modal-visible');
+            $('.js-modal-shown').removeClass('js-modal-shown');
+        }
+    }
+};
       

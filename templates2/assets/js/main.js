e72dc010f2c79fd73b25380b6113ad1bf09dee7e
Maeli Hector <maelihector@gmail.com>
2018-03-20 19:56:21 -0500

    Seperate templates to work on contact.html and search.html.

---
+++ templates2/assets/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+
+
+  (function ($) {
+    $(function () {
+
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
+
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+
+
+  // Modal function
+
+  $(document).ready(function () {
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+
+  $(document).ready(function () {
+    $('.carousel').carousel();
+  });
+
+  $(document).ready(function () {
+    $('.collapsible').collapsible();
+  });
+
+  $("#sendMessage").on("click", function (e) {
+
+
+  });
+
+})  

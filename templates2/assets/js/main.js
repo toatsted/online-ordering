43ca8672af55fe970160a740b2815830bb59e5f4
Maeli Hector <maelihector@gmail.com>
2018-03-21 23:40:32 -0500

    Add hrefs and id's to jump to certain part of the page.

--- templates2/assets/js/main.js
+++ templates2/assets/js/main.js
@@ -32,3 +32,22 @@ $(document).ready(() => {
   $(".button-collapse").sideNav();
 
 })  
+
+$('#mobile-nav-trigger').on('click', function() {
+  console.log("hello");
+  $('.navigation').css({'-webkit-transform': 'translateX(0%)', 'transform': 'translateX(0%)'});
+  $('body').addClass('js-modal-visible');
+  $('#mobile-nav-trigger').addClass('js-modal-shown');
+});
+
+$('.js-modal-shown').on('click', function() {
+  
+  console.log("ldffol");
+  $('.navigation').css({'-webkit-transform': 'translateX(-100%)', 'transform': 'translateX(-100%)'});
+});
+
+$( document ).ready(function() {
+  console.log("lol");
+    $('.statistic__number').animate({opacity: '1'}, "slow");
+});
+
